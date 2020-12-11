1b0fba2019128cc78798c9b13b231a217c444e0f
markotre <trebulamarek1@gmail.com>
2020-12-11 17:23:04 +0100

    Last velocity updates

--- abb_scanner_pkg/src/scanner_node.cpp
+++ abb_scanner_pkg/src/scanner_node.cpp
@@ -7,13 +7,13 @@ int main(int argc, char** argv){
     ros::NodeHandle n;
 
     LaserScanToPointCloud laserScanToPointCloud;
-    //ros::Rate rate(10);
+    ros::Rate rate(100);
     ROS_INFO("Initializing scanner_node");
 
     while(ros::ok()){
         ros::getGlobalCallbackQueue()->callAvailable(ros::WallDuration(0.1));
         ros::spin();
-        //rate.sleep();
+        rate.sleep();
     }
 
     return 0;
